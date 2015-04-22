b4d1de6aa711997fbc548ed5e375f928909b84fe
laitaka777 <laitaka777@gmail.com>
2015-04-23 00:36:47 +0900

    cppとhppにソースコードを分割

--- graph.cpp
+++ graph.cpp
@@ -1,13 +1,4 @@
-#include <bits/stdc++.h>
-
-#define FOR(i,a,b) for(int i=(a);i<(b);++i)
-
-template<typename T>
-void bfs( std::vector<std::vector<T> > graph, T start_node, std::vector<T> visited_nodes );
-
-template<typename T>
-void dfs( std::vector<std::vector<T> > graph, T start_node, std::vector<T> visited_nodes );
-
+#include "graph.hpp"
 
 int main( int argc, char **argv )
 {
---
+++ graph.hpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+
+template<typename T>
+void bfs( std::vector<std::vector<T> > graph, T start_node, std::vector<T> visited_nodes );
+
+template<typename T>
+void dfs( std::vector<std::vector<T> > graph, T start_node, std::vector<T> visited_nodes );
